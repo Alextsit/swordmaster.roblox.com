51ee1b0178f150b4036bb7c6d30ef0e63ec14271
IllusionStudios <82045339+Illusionstud@users.noreply.github.com>
2024-08-05 23:44:26 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,2 +1,8 @@
+console.log("Hello World")
+function changetext() {
+  console.log("Text Changed")
+  document.getElementById("testselect").innerHTML = "Text Changed"
+  alert(`this is a test`);
+}
 
 
